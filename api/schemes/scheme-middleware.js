667dac1d861642ce3bf572fe986be2352bf7a580
yfngary <kalebglundquist@gmail.com>
2023-01-23 14:35:58 -0700

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require('../../data/db-config')
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db('schemes')
+      .where('scheme_id', req.params.scheme_id).first()
+    if (existing) {
+      next()
+    } else {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
